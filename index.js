da9d73ee953851247fb697f48fd25e3ca2392aea
Rajesh <sjha707@gmail.com>
2022-12-13 18:59:41 +0530

    Api_for_crud

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const port = 9000;
+require("dotenv").config();
+const port = process.env.PORT || 9000;
 
 const bodyParser = require("body-parser");
 
